6aa91f52844c0e6d685311d77676d50b04ed5884
Andre <andre.pbo@gmail.com>
2018-12-19 10:36:15 -0300

    Update InnerBorderKarel.java

--- InnerBorderKarel.java
+++ InnerBorderKarel.java
@@ -11,18 +11,25 @@ import stanford.karel.*;
 
 public class InnerBorderKarel extends SuperKarel {
 	public void run() {
+		startPosition();
+		for (int i = 0; i < 4; i++) {
+			makeSquare();
+		}
+	}
+	/*
+	 * Precondition: Karel starts at position (1,1)
+	 * Postcondition: Karel facing east at position (1,2)
+	 */
+	private void startPosition() {
 		turnLeft();
 		move();
 		turnRight();
-		for (int i = 0; i < 4; i++) {
-			tipOfSquare();
-		}
 	}
 	/*
 	 * Precondition: Karel facing east at position (1,2)
 	 * Postcondition: Karel standing in last beeper of the line facing next line
 	 */
-	private void tipOfSquare() {
+	private void makeSquare() {
 		while(frontIsClear()) {
 			move();
 			if(noBeepersPresent()) {
